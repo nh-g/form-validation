33ed96a77ff09ba9b5429c40a8bfc65c2da31a63
nh-g <giangngohong@gmail.com>
2021-08-21 22:57:16 +0200

    add error className to style; separate label and input properties to style

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -18,73 +18,65 @@ export default function Form({
       </div>
       <form onSubmit={handleSubmit} autoComplete="off">
         <div className="form-group">
-          <label htmlFor="first-name-input">
-            First Name *
-            <input
-              type="text"
-              className="form-control"
-              id="first-name-input"
-              placeholder="Enter first name"
-              value={values.firstName}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              name="firstName"
-              required
-            />
-            {touched.firstName && errors.firstName}
-          </label>
+          <label htmlFor="first-name-input">First Name *</label>
+          <input
+            type="text"
+            className="form-control"
+            id="first-name-input"
+            placeholder="Enter first name"
+            value={values.firstName}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            name="firstName"
+            required
+          />
+          <span className="error">{touched.firstName && errors.firstName}</span>
         </div>
         <div className="form-group">
-          <label htmlFor="last-name-input">
-            Last name *
-            <input
-              type="text"
-              className="form-control"
-              id="last-name-input"
-              placeholder="Enter last name"
-              value={values.lastName}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              name="lastName"
-              required
-            />
-            {touched.lastName && errors.lastName}
-          </label>
+          <label htmlFor="last-name-input">Last name *</label>
+          <input
+            type="text"
+            className="form-control"
+            id="last-name-input"
+            placeholder="Enter last name"
+            value={values.lastName}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            name="lastName"
+            required
+          />
+          <span className="error">{touched.lastName && errors.lastName}</span>
         </div>
         <div className="form-group">
-          <label htmlFor="email">
-            Email address *
-            <input
-              type="text"
-              className="form-control"
-              id="email"
-              placeholder="Enter email"
-              value={values.email}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              name="email"
-              required
-            />
-            {touched.email && errors.email}
-          </label>
+          <label htmlFor="email">Email address *</label>
+          <input
+            type="text"
+            className="form-control"
+            id="email"
+            placeholder="Enter email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            name="email"
+            required
+          />
+          <span className="error">{touched.email && errors.email}</span>
         </div>
         <div className="form-group">
-          <label htmlFor="age">
-            Age *
-            <input
-              type="number"
-              className="form-control"
-              id="age"
-              placeholder="Enter age"
-              value={values.age || ""}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              name="age"
-              min="0"
-              required
-            />
-            {touched.age && errors.age}
-          </label>
+          <label htmlFor="age">Age *</label>
+          <input
+            type="number"
+            className="form-control"
+            id="age"
+            placeholder="Enter age"
+            value={values.age || ""}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            name="age"
+            min="0"
+            required
+          />
+          <span className="error"> {touched.age && errors.age}</span>
         </div>
         <div className="form-group">
           <button type="submit" className="btn btn-primary">
